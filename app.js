56561637d805cf8bd18a72ba11544ba8ba5f0c46
BartaBlzs <BartaBlzs@gmail.com>
2023-03-23 17:47:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Files
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+// Set views
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index', {text: 'this is ejs'})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+
+
+app.listen(port, ()=>console.info('App available on localhost '+port))
